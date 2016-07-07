fb6c5a9a2715cb9003bd8e901764ca3c9de46097
israel <israel.berbigier@gmail.com>
2016-07-07 15:23:45 -0300

    commit

--- apm-arq4/src/main/java/com/easyguinchoservice/apm/guincho/GuinchoBD.java
+++ apm-arq4/src/main/java/com/easyguinchoservice/apm/guincho/GuinchoBD.java
@@ -3,27 +3,30 @@ package com.easyguinchoservice.apm.guincho;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.inject.Inject;
 import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 import javax.persistence.Query;
 
 import com.easyguinchoservice.apm.ed.GuinchoED;
 
 public class GuinchoBD {
-	@Inject
-	EntityManager em ; 
+	
+	EntityManagerFactory factory = Persistence.createEntityManagerFactory("minhaapppersistenceunit");
+	EntityManager entityManager = factory.createEntityManager();
+	
+
 	public List<GuinchoED> listaGuinchos() {
 		List<GuinchoED> listaGuinchos = new ArrayList<GuinchoED>();
-		//TODO mocar dados
 		return listaGuinchos;
 	}
 	
 	public void inclui(GuinchoED guincho) {
-		 em.persist(guincho);
+		entityManager.persist(guincho);
 	}
 	
 	public List<GuinchoED> listaTodosGuinchos() {
-		Query result = (Query) em.createNativeQuery("SELECT g FROM GuinchoED g");
+		Query result = (Query) entityManager.createNativeQuery("SELECT g FROM GuinchoED g");
 		return result.getResultList();
 	}
 	
@@ -34,11 +37,11 @@ public class GuinchoBD {
 		Double longMais = longi + 100;
 		Double latMais = lati + 100;
 		
-		Query query = (Query) em.createNativeQuery("SELECT g FROM GuinchoED g WHERE g.longitude > :longMenos "
+		Query query = (Query) entityManager.createNativeQuery("SELECT g FROM GuinchoED g WHERE g.longitude > :longMenos "
 				+ "and g.longitude < :longMais and g.latitude > :latiMenos and g.latitude < :latiMais");
 		
 		query.setParameter("longMenos", longMenos);
-		query.setParameter("longMais", longMenos);
+		query.setParameter("longMais", longMais);
 		query.setParameter("latiMenos", latMenos);
 		query.setParameter("latiMais", latMais);
 		
